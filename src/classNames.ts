a456133f59d6523bf31cb6fbf8d829d84a0ea109
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-10-21 11:23:35 +0200

    Adds classNames

---
+++ src/classNames.ts
@@ -0,0 +1,11 @@
+export const classNames = {
+  content: 'content',
+  label: 'label',
+  link: 'link',
+  modal: 'modal',
+  button: 'button',
+  iconButton: 'button-icon',
+  iconLink: 'button-icon',
+  typography: 'typography',
+  box: 'box',
+};
