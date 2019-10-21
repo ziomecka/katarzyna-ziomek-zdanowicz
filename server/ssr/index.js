0a73c17ebb59c192335ef953b3b9468aea4861a2
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-10-21 13:21:16 +0200

    Corrects path in server ssr

--- server/ssr/index.js
+++ server/ssr/index.js
@@ -6,13 +6,13 @@ const { saveToFile } = require('./save-to-file');
 const { NODE_ENV } = process.env;
 
 const bundlePath = NODE_ENV === 'production'
-  ? '../../bundleServerProd/'
-  : '../../bundleServerDev/';
+  ? '../../bundleServerProd'
+  : '../../bundleServerDev';
 
 console.log(path.resolve(__dirname, bundlePath), 'bundlePath')
 module.exports = new Promise(() => {
-  require(path.resolve(__dirname, bundlePath, '/server.js')).default.then((prepareHtml) => {
     console.log('tries to save to bundelPath', path.resolve(__dirname, bundlePath, 'template.html'));
+  require(path.join(bundlePath, '/server.js')).default.then((prepareHtml) => {
     console.log(typeof prepareHtml)
     saveToFile({
       filePath: path.resolve(__dirname, bundlePath, 'index.html'),
