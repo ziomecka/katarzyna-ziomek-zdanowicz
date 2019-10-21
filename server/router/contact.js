22f970c944a83b8b134859b743b2b0b9af6ff739
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-10-21 10:59:07 +0200

    Server | adds router

---
+++ server/router/contact.js
@@ -0,0 +1,36 @@
+const sendEmail = require('../email/');
+
+const emailRegexp = /\w+/gi;
+const nameRegexp = /\w+/gi;
+
+const isValid = (email, regExp) => (
+  regExp.test(email)
+);
+
+const handler = async ({ query }, res, next) => {
+  const { email, name } = query;
+
+  if (isValid(email, emailRegexp) && isValid(name, nameRegexp)) {
+    try {
+      await sendEmail({
+        recipients: [ { address: email } ],
+        emailVariant: 'thankYou',
+      });
+      res.send({ result: true });
+    } catch (err) {
+      res.send({
+        result: false,
+        err,
+      });
+    }
+  } else {
+    res.send({
+      result: false,
+      err: new Error('Invalid data'),
+    });
+  }
+
+  next();
+};
+
+module.exports = handler;
---
+++ server/router/index.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+
+const express = require('express');
+const path = require('path');
+const router = express.Router();
+const contact = require('./contact');
+
+const STATIC_PATH = process.env.NODE_ENV === 'production'
+  ? path.resolve(__dirname, '../../bundleServerProd')
+  : path.resolve(__dirname, '../../bundleServerDev');
+
+const HTML_PATH = path.resolve(STATIC_PATH, './index.html');
+
+router.use(express.static(STATIC_PATH));
+
+router.get('*.js', (req, res) => {
+  res.set('Content-Type', 'text/javascript');
+  res.setHeader('Content-Encoding', 'gzip');
+  res.sendFile(path.join(STATIC_PATH, `${ req.url }.gz`));
+});
+
+router.get('/', (req, res) => {
+  res.sendFile(HTML_PATH);
+});
+
+router.get('/contact*', contact);
+
+module.exports = router;
+
