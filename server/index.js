071c3ca56abac2098a71147bb0a60130f19344de
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-10-21 11:00:03 +0200

    Server | adds index.js

---
+++ server/index.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+require('./ssr/');
+
+const cors = require('cors');
+const express = require('express');
+const helmet = require('helmet');
+const csp = require('helmet-csp');
+const http = require('http');
+const router = require('./router/index.js');
+const getCspDirectives = require('./get-csp-directives');
+const app = express();
+const server = http.Server(app);
+
+const port = process.env.PORT || 3000;
+
+app.set('trust proxy', 1);
+app.use(cors({
+  exposedHeaders: [ 'Content-Type', 'Content-Security-Policy' ],
+  credentials: true,
+  origin: true,
+}));
+
+app.use(helmet());
+app.use(csp({ directives: getCspDirectives() }));
+app.use(router);
+
+server.listen(port, () => console.log(`Listening on port: ${ port }`)); // eslint-disable-line
