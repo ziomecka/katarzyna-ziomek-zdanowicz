87fd380fa0fa8204386e58be513de8a7aeebf523
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-10-21 11:54:24 +0200

    Deletes email on server side

--- server/email/constants.js
+++
@@ -1,9 +0,0 @@
-const addressFrom = 'hello';
-const domain = 'letsbitebytes.com';
-const name = 'Katarzyna Ziomek-Zdanowicz';
-
-module.exports = {
-  addressFrom,
-  domain,
-  name,
-};
--- server/email/content/html.js
+++
@@ -1,1 +0,0 @@
-module.exports = value => `<html><body>${ value }</body></html>`;
