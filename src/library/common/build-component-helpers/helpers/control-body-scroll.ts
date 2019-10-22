25316ba13cfca8d65e4290c64efc4098cce2d946
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-10-22 08:16:37 +0200

    Removes adding paddingRight whe turningOff body scrolling

--- src/library/common/build-component-helpers/helpers/control-body-scroll.ts
+++ src/library/common/build-component-helpers/helpers/control-body-scroll.ts
@@ -1,15 +1,10 @@
 const turnOnBodyScrolling = (): void => {
-  document.body.style.paddingRight = '0';
   document.body.style.overflowY = 'visible';
 };
 
 const turnOffBodyScrolling = (): void => {
-  const { body } = document;
-  // paddingRight of body is set to scrollBar width
-  body.style.paddingRight = (innerWidth - body.offsetWidth).toString();
-  body.style.overflowY = 'hidden';
+  document.body.style.overflowY = 'hidden';
 };
-
 
 export default {
   turnOnBodyScrolling,
