43c1d58db79e0c338cb8e66ba9bfbc83d5af3d72
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-10-26 19:55:47 +0200

    Uses minify-html in ssr

--- server/ssr/save-to-file.js
+++ server/ssr/save-to-file.js
@@ -1,8 +1,22 @@
 const fs = require('fs');
+const { minify } = require('html-minify');
 
 const saveToFile = async ({ filePath, content }) => {
+  const minified = minify(content, {
+    collapseBooleanAttributes: true,
+    collapseInlineTagWhitespace: true,
+    collapseWhitespace: true,
+    conservativeCollapse: true,
+    minifyCSS: true,
+    removeRedundantAttributes: true,
+    removeScriptTypeAttributes: true,
+    removeStyleLinkTypeAttributes: true,
+    removeTagWhitespace: true,
+    useShortDoctype: true,
+  });
+
   return new Promise((resolve, reject) => {
-    fs.writeFile(filePath, content, (err, result) => {
+    fs.writeFile(filePath, minified, (err, result) => {
       if (err) reject(err);
       resolve(result);
     });
