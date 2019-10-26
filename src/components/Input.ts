10187a92dd2d0b50b8a316101b5d2481f63f02cd
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-10-26 09:00:36 +0200

    Uses InputComponentBuilder in Input component

--- src/components/Input.ts
+++ src/components/Input.ts
@@ -1,54 +1,14 @@
 import {
-  Box,
-  InputLabel,
-} from './';
+  InputComponentBuilder,
+  InputType,
+} from '../builders/';
 
-const defaultType = 'text';
-
-export const Input: ComponentFunction<InputProps> = (
-  ({
-    type = defaultType,
-    label,
-    id = '',
-    placeholder,
-    required = true,
-    attributes: { tabIndex = -1, autoFocus = false, ...otherAttributes } = {},
-    ...otherProps
-  } = {}) => {
-
-    const InputComponent = ({
-      attributes = {},
-      ...other
-    }: Partial<InputProps> & Partial<ComponentProps> = {}): string => (
-      createComponent({
-        HTMLTag: 'input',
-        attributes: {
-          type,
-          id,
-          placeholder,
-          required,
-          ...attributes,
-          ...otherAttributes,
-        },
-        ...otherProps,
-        ...other,
-      })
-    );
-
-    return label
-      ? Box({
-        flex: {
-          display: 'flex',
-          justifyContent: 'center',
-        },
-        attributes: { tabIndex, autoFocus },
-        className: 'box-input',
-        children: [
-          InputComponent(),
-          InputLabel({ label, autoFocus }),
-        ],
-      })
-      : InputComponent({ tabIndex, autoFocus });
+export const Input: ComponentFunction<InputProps> =
+  InputComponentBuilder({
+    HTMLTag: 'input',
+    attributes: {
+      type: InputType.text,
+    },
   });
 
 interface InputProps {
