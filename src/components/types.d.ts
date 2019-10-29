50de846fe803780ba8d8c7e8e1e4d9bfa08bf373
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-10-29 08:26:27 +0100

    Adds imports of types from library render-it

--- src/components/types.d.ts
+++ src/components/types.d.ts
@@ -1,51 +1,48 @@
-declare type ClassNames = Record<string, string> & {
-  main?: string;
-  header?: string;
-  footer?: string;
-  content?: string;
-  heading?: string;
-  paper?: string;
-  input?: string;
-  button?: string;
-}
+declare type ClassNames =
+  import('../../../just/devBundle/client/types.js').ClassNames & {
+    main?: string;
+    header?: string;
+    footer?: string;
+    content?: string;
+    heading?: string;
+    paper?: string;
+    input?: string;
+    button?: string;
+  };
 
-declare type Flex = {
-  display?: 'flex' | 'inline-flex';
-  justifyContent?: 'center' |
-    'flex-start' | 'flex-end' | 'space-between' | 'space-around';
-  alignItems?: 'center' | 'flex-start' | 'flex-end';
-  flexDirection?: 'column' | 'row';
-  flexWrap?: 'nowrap' | 'wrap';
-}
-
-declare type Target = '_blank' | '';
-
-declare type LinkAttributes = ElementAttributes & {
-  target?: Target;
-  href: string;
-  download?: boolean;
-}
-
-declare type ElementAttributes = {
-  id?: string;
-  title?: string;
-  tabIndex?: number;
-  autoFocus?: boolean;
-  'aria-hidden'?: boolean;
-};
+declare type LinkAttributes = import(
+  '../../../just/devBundle/client/types.js'
+).HTMLAnchorElementAttributes;
 
 type CustomHTMLElementProps = import(
 'rendering-recursive-server-side'
-).CustomHTMLElementProps
+).CustomHTMLElementProps;
 
 declare type HTMLElementProps = CustomHTMLElementProps & {
   classNames?: ClassNames;
-  flex?: Flex;
 };
 
-declare type ComponentProps = import('../library/common/').ComponentProps;
+declare type HTMLInputType =
+  import('../../../just/devBundle/client/types.js').HTMLInputType;
+
+type F = import('../../../just/devBundle/client/types.js').Flex;
+
+declare interface Flex extends F {}
+
+declare type HTMLElementAttributes =
+  import('../../../just/devBundle/client/types.js').HTMLElementAttributes;
+
+declare type HTMLInputElementAttributes =
+  import('../../../just/devBundle/client/types.js').HTMLInputElementAttributes;
+
+declare type HTMLTextareaElementAttributes =
+  import('../../../just/devBundle/client/types.js')
+    .HTMLTextareaElementAttributes;
+
+declare type ComponentProps =
+  import('../../../just/devBundle/client/types.js').ComponentProps;
+
 
 declare type ComponentFunction<P = ComponentProps, R = string> =
-  import('../library/common/')
-  .ComponentFunction<Partial<ComponentProps> & P, R>;
-
+  import('../../../just/devBundle/client/types.js')
+  .ComponentFunction<P, R>;
