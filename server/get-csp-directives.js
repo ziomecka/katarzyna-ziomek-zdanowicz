fe99f4d35629ec5d1c11a5a5c7cb01a01561c315
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-10-30 15:04:55 +0100

    Deletes origin from CSP, as default self is for the same origin

--- server/get-csp-directives.js
+++ server/get-csp-directives.js
@@ -2,31 +2,25 @@
 require('dotenv').config();
 
 const getCspDirectives = () => {
-  const origin = process.env.NODE_ENV === 'production'
-    ? "'https://katarzyna-ziomek-zdanowicz.com'"
-    : "'http://localhost:3000'";
   const self = "'self'";
   const unsafeInline = "'unsafe-inline'";
   const images = [
     'https:',
     'data:',
-    origin,
   ];
   const fonts = [
     'data:',
-    origin,
     self,
   ];
 
   return {
     defaultSrc: [ self ],
-    connectSrc: [ self, origin ],
+    connectSrc: [ self ],
     fontSrc: [ ...fonts ],
-    imgSrc: [ self, origin, ...images ],
-    manifestSrc: [ self, origin ],
-    // todo add nonce to inline script and remove unsafe inline
-    scriptSrc: [ self, origin, unsafeInline ],
-    styleSrc: [ self, origin, unsafeInline ],
+    imgSrc: [ self, ...images ],
+    manifestSrc: [ self ],
+    scriptSrc: [ self ],
+    styleSrc: [ self, unsafeInline ],
   };
 };
 
