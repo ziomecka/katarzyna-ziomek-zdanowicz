3c05307f2bfceee5288be853925835766c0d1b64
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-11-05 09:42:45 +0100

    Adds @babel/preset-typescript and @babel/transform-runtime to babel.config

--- babel.config.js
+++ babel.config.js
@@ -2,12 +2,14 @@ module.exports = (api) => {
   api.cache(false);
 
   const presets = [
+    '@babel/preset-typescript',
     '@babel/preset-env',
   ];
 
   const plugins = [
     '@babel/plugin-transform-for-of',
     '@babel/plugin-proposal-class-properties',
+    '@babel/transform-runtime',
   ];
 
   return { presets, plugins };
